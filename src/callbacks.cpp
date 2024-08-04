d87c01b3c080ce875a1a9fbc13c4ecd2270d986e
piop2 <pioooop2@gmail.com>
2024-08-05 01:19:56 +0900

    add glfw frame buffer size callback

--- src/callbacks.cpp
+++ src/callbacks.cpp
@@ -1,5 +1,10 @@
 #include "callbacks.hpp"
 
+#include <string>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include "utilities.hpp"
 
 void GLFWErrorCallback(int error, const char* description)
@@ -11,4 +16,11 @@ void GLFWErrorCallback(int error, const char* description)
 void GLFWWindowCloseCallback(GLFWwindow* window)
 {
 	WriteLog("window closed");
+}
+
+void GLFWFramebufferSizeCallback(GLFWwindow* window, int w, int h)
+{
+	std::string the_log = "window size changed to: " + std::to_string(w) + "x" + std::to_string(h);
+	WriteLog(the_log.c_str());
+	glViewport(0, 0, w, h);
 }
--- src/callbacks.hpp
+++ src/callbacks.hpp
@@ -2,10 +2,13 @@
 #define OPENGLDEMO_CALLBACKS_H
 #pragma once
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 void GLFWErrorCallback(int error, const char* description);
 
 void GLFWWindowCloseCallback(GLFWwindow* window);
+
+void GLFWFramebufferSizeCallback(GLFWwindow* window, int w, int h);
 
 #endif // OPENGLDEMO_CALLBACKS_H
