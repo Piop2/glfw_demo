a67457861cf5448d4f7b3400ea52b70f6fa53941
piop2 <pioooop2@gmail.com>
2024-08-05 01:21:55 +0900

    add shader loader

---
+++ src/rendering/gl/shader_loader.hpp
@@ -0,0 +1,10 @@
+#if !defined(OPENGLDEMO_RENDERING_GL_SHADERLOADER_H)
+#define OPENGLDEMO_RENDERING_GL_SHADERLOADER_H
+
+#include <vector>
+
+#include <glad/glad.h>
+
+GLuint LoadShader(const char* vertexshader, const char* fragmentshader);
+
+#endif // OPENGLDEMO_RENDERING_GL_SHADERLOADER_H
