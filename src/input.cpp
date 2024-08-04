1439c6c762947c61af0fae05e8e75f1d310512a7
piop2 <pioooop2@gmail.com>
2024-08-05 01:19:37 +0900

    add input callback function

---
+++ src/input.cpp
@@ -0,0 +1,11 @@
+#include "input.hpp"
+
+#include <GLFW/glfw3.h>
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
---
+++ src/input.hpp
@@ -0,0 +1,8 @@
+#if !defined(OPENGLDEMO_INPUT_H)
+#define OPENGLDEMO_INPUT_H
+
+#include <GLFW/glfw3.h>
+
+void ProcessInput(GLFWwindow* window);
+
+#endif // OPENGLDEMO_INPUT_H
