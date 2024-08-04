ece8e1b254e4bead73642c4a7531f33508f5bd47
piop2 <pioooop2@gmail.com>
2024-08-05 01:20:50 +0900

    add opengl glsl vertion print function

---
+++ src/rendering/gl/version.cpp
@@ -0,0 +1,25 @@
+#include "version.hpp"
+
+#include <cstdio>
+
+#include <glad/glad.h>
+
+void printGLinfo()
+{
+    const GLubyte* vendor = glGetString(GL_VENDOR);
+    printf("GL Vendor : %s\n", vendor);
+
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    printf("GL Renderer : %s\n", renderer);
+
+    const GLubyte* version = glGetString(GL_VERSION);
+    printf("GL Version (string) : %s\n", version);
+
+    GLint major, minor;
+    glGetIntegerv(GL_MAJOR_VERSION, &major);
+    glGetIntegerv(GL_MINOR_VERSION, &minor);
+    printf("GL Version (integer) : %d.%d\n", major, minor);
+
+    const GLubyte* glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    printf("GLSL Version : %s\n", glslVersion);
+}
